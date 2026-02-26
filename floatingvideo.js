9144e5b99a4c3a615fa03591df5a1609d78ce93e
quanghy-hub <dr.huy93@gmail.com>
2026-02-26 19:12:16 +0700

    up

--- floatingvideo.js
+++ floatingvideo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Floating
 // @namespace    
-// @version      5.9.14
+// @version      5.9.15
 // @description  Floating video player optimized for mobile with video rotation
 // @author       Claude
 // @match        *://*/*
